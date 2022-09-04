3e59b9c245a83751fa97143059de36e3bdd6399c
Bojan Mrkaja <39374674+BojanMrkaja@users.noreply.github.com>
2022-09-04 18:58:28 +0200

    What is JavaScript??

---
+++ 08-Behind-the-Scenes/starter/script.js
@@ -0,0 +1,46 @@
+/*
+Java Script je:
+
+1.Hight- level programski jezik
+Svakom programu su potrebni resursi racunara kao sto su memorija ili CPU(procesor).
+U ovom smislu high-level znaci da programer ili developer ne mora  manuelno da upravlja resursima 
+na primer ne mora da "memoriju" za prostor da bi kreirao varijablu vec se to automacki radi u pozadini tj. u javaScript engin-u.
+
+Postoje i low-level programski jezici kao sto su C gde diveloper manuelno upravlja resursima.
+Rezultat toga je da su programi pisani u C ili nekom drugom low-level jeziku mnogo brzi i optimizovaniji
+nego programi pisani u high-level jezicima. 
+
+
+2.Garbage-collected
+Je u sustini algoritam u JavaScript enginu koji cisti stare i nepotrebne objekte iz memorije
+
+
+3.Interpreted or just-in-time compiled
+Sa obzirom da CPU(procesor) razume samo nule i jedinice svaki program mora da bude napisan sa jedinicam i nulama.
+Koji se drugacije naziva machine code ili masinski kod.Sa obzirom da masinski kod nije praktican za pisanje mi pisemo kod 
+koji je razumljiv.Ali taj kod mora da se prevede u masinski kod kako bi ga racunar razumeo.U slucaju JavaScripta ovo se desava
+u JavaScript engin-u.
+
+
+4.Multi-paradigm
+U programiranju paradigm je sveobuhvatni pristup strukturisanju koda.
+
+Tri popularna paradigma su:
+-Procedural programmin
+-Object-oriented programming
+-Functional programming
+
+
+5.Prototype-based object-oriented
+Kao prvo skoro sve u JavaScriptu je objekat sem primitivnih tipova podataka kao sto su brojevi, stringovi itd.
+Ali na primer nizovi(array's) su objekti u stvari a ne poseban tip podatka.
+Mozemo da koristimo razne array metode kao sto su push ili indexOf zahvaljujuci prototype inheritance sistemu.
+U sustini mi kreiramo nizove(array's) iz Array sablona koji se naziva prototype.
+Ovaj Prototype sadrzi sve array metode i onda nizovi(array's) u nasem kodu nasledjuju sve metode Array Prototype-a.
+
+
+6.First-class functions
+JavaScript je jezik sa first-class funkcijamasto znaci da se funkcije tretiraju kao varijable.
+Sto znaci da mozemo da ih dodajemo kao argumente u druge funkcije ili da ih vratimo(return) iz drugih funkcija.
+Ovo nam omogucava da koristimo functional programming
+ */
