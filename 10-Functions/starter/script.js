5c32ca938a579702d096b1698afc29b6ee3f528e
Bojan Mrkaja <39374674+BojanMrkaja@users.noreply.github.com>
2022-10-02 14:10:45 +0200

    VALUE VS. REFERENCE

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -16,8 +16,37 @@ const createBooking = function (
   };
 
   bookings.push(booking);
-  console.log(bookings);
+  //   console.log(bookings);
 };
 
 createBooking('LH123');
 createBooking('LH213', 32, 999);
+
+//HOW PASSING ARGUMENTS WORKS: VALUE VS. REFERENCE
+
+const flight = 'LH213';
+const bojan = {
+  name: 'Bojan Mrkaja',
+  passport: 123475986,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LJ444';
+  passenger.name = `Mr. ${passenger.name}`;
+
+  //   passenger.passport === 1234759869
+  //     ? alert('check in')
+  //     : alert('Wrong passport');
+};
+
+// checkIn(flight, bojan);
+
+// console.log(flight);
+// console.log(bojan);
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 100000);
+};
+
+newPassport(bojan);
+checkIn(flight, bojan);
