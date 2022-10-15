a9c0acb4b909f2a5da9894913bff8826451b41ce
Bojan Mrkaja <39374674+BojanMrkaja@users.noreply.github.com>
2022-10-15 19:32:26 +0200

    MATH AND ROUNDING

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -94,7 +94,7 @@ const displayMovements = function (movements, sort = false) {
         <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
-        <div class="movements__value">${mov}€</div>
+        <div class="movements__value">${mov.toFixed(2)}€</div>
       </div>
     `;
 
@@ -104,19 +104,19 @@ const displayMovements = function (movements, sort = false) {
 
 const calcDisplayBalance = function (acc) {
   acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${acc.balance}€`;
+  labelBalance.textContent = `${acc.balance.toFixed(2)}€`;
 };
 
 const calcDisplaySummary = function (acc) {
   const incomes = acc.movements
     .filter(mov => mov > 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumIn.textContent = `${incomes}€`;
+  labelSumIn.textContent = `${incomes.toFixed(2)}€`;
 
   const out = acc.movements
     .filter(mov => mov < 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumOut.textContent = `${Math.abs(out)}€`;
+  labelSumOut.textContent = `${Math.abs(out.toFixed(2))}€`;
 
   const interest = acc.movements
     .filter(mov => mov > 0)
@@ -126,7 +126,7 @@ const calcDisplaySummary = function (acc) {
       return int >= 1;
     })
     .reduce((acc, int) => acc + int, 0);
-  labelSumInterest.textContent = `${interest}€`;
+  labelSumInterest.textContent = `${interest.toFixed(2)}€`;
 };
 
 const createUsernames = function (accs) {
@@ -206,7 +206,7 @@ btnTransfer.addEventListener('click', function (e) {
 btnLoan.addEventListener('click', function (e) {
   e.preventDefault();
 
-  const amount = +inputLoanAmount.value;
+  const amount = Math.floor(inputLoanAmount.value);
 
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
     // Add movement
@@ -272,3 +272,30 @@ console.log(Number.isFinite(30));
 console.log(Number.isFinite('33'));
 console.log(Number.isInteger(22));
 console.log(Number.isInteger('33'));
+
+//MATH AND ROUNDING
+console.log(Math.sqrt(25));
+
+//max value
+console.log(Math.max(12, 3, 55, 6, 7));
+//min value
+console.log(Math.min(12, 3, 55, 6, 7));
+
+//calculate area of circle
+console.log(Math.PI * Number.parseFloat('10px') ** 2);
+
+//random numbers
+console.log(Math.trunc(Math.random() * 6) + 1);
+
+const random = (min, max) => Math.floor(Math.random() * (max - min) + 1) + min;
+
+console.log(random(10, 20));
+
+//rounding integers
+console.log(Math.trunc(23.22));
+console.log(Math.ceil(23.8));
+console.log(Math.floor(22.44));
+
+//rounding decimals
+console.log((2.5).toFixed(1));
+console.log((2.3).toFixed(3));
