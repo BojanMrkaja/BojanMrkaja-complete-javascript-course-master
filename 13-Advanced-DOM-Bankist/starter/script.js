50f69a396f53ef27176e097aae7664b54ae10c9e
Bojan Mrkaja <39374674+BojanMrkaja@users.noreply.github.com>
2022-10-19 20:19:02 +0200

    create, insert and delete elemnts in DOM tree

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -8,7 +8,8 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function () {
+const openModal = function (e) {
+  e.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -18,8 +19,7 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
@@ -29,3 +29,68 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+///////////////////////////////////////
+/*
+The HTML DOM (Document Object Model)
+Kada se veb stranica učita, pretraživač kreira model objekta dokumenta stranice.
+HTML DOM model je konstruisan kao stablo objekata.
+
+Sa objektnim modelom, JavaScript dobija svu mogucnost koja mu je potrebna za kreiranje dinamičkog HTML-a:
+
+-JavaScript može da promeni sve HTML elemente na stranici
+-JavaScript može da promeni sve HTML atribute na stranici
+-JavaScript može da promeni sve CSS stilove na stranici
+-JavaScript može ukloniti postojeće HTML elemente i atribute
+-JavaScript može dodati nove HTML elemente i atribute
+-JavaScript može da reaguje na sve postojeće HTML event-e na stranici
+-JavaScript može da kreira nove HTML event-e na stranici
+
+DOM je veoma kompleksan API koji sadrzi mnogo metoda i properties-a za interakciju sa DOM stablom(tree):
+.querySelector()
+.addEventListener()
+.createElemnt()
+.innerHTML()
+.textContent()
+.children()
+  etc.
+*/
+
+console.log(document.documentElement);
+
+const header = document.querySelector('.header');
+const allSection = document.querySelectorAll('.section'); //return node list
+
+console.log(allSection);
+
+document.getElementById('section--1');
+
+const allBtns = document.getElementsByTagName('button'); //return HTML Collection
+//HTML collection se azurira u realnom vremenu za razliku od node liste
+
+console.log(allBtns);
+
+const btnClass = document.getElementsByClassName('btn'); //return HTML Collection
+console.log(btnClass);
+
+//Creating and inserting elements
+
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+message.innerHTML = `
+We use cookies for improved functionality and analitycs
+<button class="btn btn-close-cookie">Got it!</button>
+`;
+
+header.prepend(message);
+// header.append(message);
+
+//da ubacimo isti DOM element na vise mjesta
+header.append(message.cloneNode(true));
+
+//delete elements
+document
+  .querySelector('.btn-close-cookie')
+  .addEventListener('click', function () {
+    message.remove();
+  });
