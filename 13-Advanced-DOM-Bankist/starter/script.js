1b38ed3f441dbc10d9c9eb491af5d2fdb53d11fa
Bojan Mrkaja <39374674+BojanMrkaja@users.noreply.github.com>
2022-10-20 18:31:31 +0200

    EventListeners

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -154,3 +154,11 @@ btnScrollTo.addEventListener('click', function (e) {
 
   section1.scrollIntoView({ behavior: 'smooth' });
 });
+
+//addEventListeners
+
+const h1 = document.querySelector('h1');
+
+h1.addEventListener('mouseenter', function (e) {
+  alert('addEventListener: Great');
+});
