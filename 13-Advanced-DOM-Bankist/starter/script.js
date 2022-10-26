d60323b11ae8c8cefa8667e025edef5e1b03f256
Bojan Mrkaja <39374674+BojanMrkaja@users.noreply.github.com>
2022-10-26 20:53:50 +0200

    slider done

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -183,6 +183,76 @@ allImg.forEach(img => {
   imgObserver.observe(img);
 });
 
+//Slider
+const slides = document.querySelectorAll('.slide');
+const btnLeft = document.querySelector('.slider__btn--left');
+const btnRight = document.querySelector('.slider__btn--right');
+const dotContainer = document.querySelector('.dots');
+let currentSlide = 0;
+const maxSlide = slides.length;
+
+const createDots = function () {
+  slides.forEach(function (_, index) {
+    dotContainer.insertAdjacentHTML(
+      'beforeend',
+      `<button class="dots__dot" data-slide="${index}"></button>`
+    );
+  });
+};
+
+createDots();
+const dotActive = function (slide) {
+  const dots = document.querySelectorAll('.dots__dot');
+  dots.forEach(dot => dot.classList.remove('dots__dot--active'));
+  const currentDot = document.querySelector(`[data-slide = '${slide}']`);
+  currentDot.classList.add('dots__dot--active');
+};
+
+dotActive(0);
+
+slides.forEach(
+  (slide, index) => (slide.style.transform = `translateX(${100 * index}%)`)
+);
+
+const nextSlide = function () {
+  currentSlide === maxSlide - 1 ? (currentSlide = 0) : currentSlide++;
+  slides.forEach(
+    (slide, index) =>
+      (slide.style.transform = `translateX(${100 * (index - currentSlide)}%)`)
+  );
+
+  dotActive(currentSlide);
+};
+
+const prevSlide = function () {
+  currentSlide === 0 ? (currentSlide = maxSlide - 1) : currentSlide--;
+  slides.forEach(
+    (slide, index) =>
+      (slide.style.transform = `translateX(${100 * (index - currentSlide)}%)`)
+  );
+  dotActive(currentSlide);
+};
+
+btnRight.addEventListener('click', nextSlide);
+
+btnLeft.addEventListener('click', prevSlide);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'ArrowLeft') prevSlide();
+  if (e.key === 'ArrowRight') nextSlide();
+});
+
+dotContainer.addEventListener('click', function (e) {
+  if (e.target.classList.contains('dots__dot')) {
+    const dataSlide = e.target.dataset.slide;
+    dotActive(dataSlide);
+    slides.forEach(
+      (slide, index) =>
+        (slide.style.transform = `translateX(${100 * (index - dataSlide)}%)`)
+    );
+  }
+});
+
 ///////////////////////////////////////
 /*
 The HTML DOM (Document Object Model)
