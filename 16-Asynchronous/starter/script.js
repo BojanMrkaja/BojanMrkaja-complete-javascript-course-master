2cb13572b35dfd4d060db5aac35b27dbac8eabb6
Bojan Mrkaja <39374674+BojanMrkaja@users.noreply.github.com>
2022-11-11 15:17:13 +0100

    first AJAX call

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -4,3 +4,38 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+const getCountryData = function (country) {
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+    const flag = data.flags.png;
+    const name = data.name.common;
+    const region = data.region;
+    const population = (+data.population / 1000000).toFixed(1);
+    const lang = Object.values(data.languages)[0];
+    const currency = Object.values(data.currencies)[0].name;
+
+    const html = `
+        <article class="country">
+          <img class="country__img" src="${flag}" />
+          <div class="country__data">
+            <h3 class="country__name">${name}</h3>
+            <h4 class="country__region">${region}</h4>
+            <p class="country__row"><span>👫</span>${
+              population > 1 ? population + 'M' : population + 'K'
+            }</p>
+            <p class="country__row"><span>🗣️</span>${lang}</p>
+            <p class="country__row"><span>💰</span>${currency}</p>
+          </div>
+        </article>
+  `;
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = '1';
+  });
+};
+
+getCountryData('serbia');
+getCountryData('usa');
