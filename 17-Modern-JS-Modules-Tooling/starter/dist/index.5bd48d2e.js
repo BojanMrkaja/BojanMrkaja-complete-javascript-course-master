e17e5886ab2e093a76b3febb5ca57d56288582f0
Bojan Mrkaja <39374674+BojanMrkaja@users.noreply.github.com>
2022-11-16 22:08:59 +0100

    forkify app init commit

--- 17-Modern-JS-Modules-Tooling/starter/dist/index.5bd48d2e.js
+++
@@ -1,2 +0,0 @@
-console.log("exporting module");const o=[];function l(l,e){o.push({product:l,quantity:e}),console.log(`${e} ${l} added to cart`)}console.log("Boris"),l("pizza",2),l("bread",3),l("apples",5),console.log(o);
-//# sourceMappingURL=index.5bd48d2e.js.map
